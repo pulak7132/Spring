99ed030a5db8caad30bc3013dca6c05137670f9e
pulak7132 <pulakranchal@outlook.com>
2020-07-14 12:00:34 -0400

    adding base entity. Closes #10.

--- spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Person.java
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.pulak.springframework.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     String firstName;
     String lastName;
 
--- spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Pet.java
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.pulak.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     Owner owner;
     LocalDate birthDate;
