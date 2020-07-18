daf2afff6b6958b554dbbcc2386605ab724acf34
pulak7132 <pulakranchal@outlook.com>
2020-07-18 19:05:14 -0400

    1. Changed AbstractMapService to implement CrudService interface. 2. Update map implementation to manage setting of the Id property. 2. Closes #23.

--- spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/services/map/AbstractMapService.java
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/services/map/AbstractMapService.java
@@ -1,30 +1,45 @@
 package com.pulak.springframework.petclinic.services.map;
 
+import com.pulak.springframework.petclinic.model.BaseEntity;
+import com.pulak.springframework.petclinic.services.CrudService;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class AbstractMapService <T, ID> {
+public class AbstractMapService <T extends BaseEntity, ID extends Number> implements CrudService <T, ID>{
     protected Map<ID, T> map = new HashMap<>();
-    Set<T> findAll(){
+    private long keyGen = 0L;
+    public Set<T> findAll(){
         return new HashSet<>(map.values());
     }
 
-    T findById(ID id){
+    public T findById(ID id){
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id,object);
+    public T save(T object){
+        if(object.getId() == null){
+            object.setId(getNewId());
+            map.put((ID)object.getId(), object);
+        }
         return object;
     }
 
-    void deleteById(ID id){
+    private Long getNewId() {
+        return ++keyGen;
+//        if(map.isEmpty())
+//            return 1L;
+//        else
+//           return  Collections.max(map.keySet()) + 1L;
+    }
+
+    public void deleteById(ID id){
         map.remove(id);
     }
 
-    void delete(T object){
+    public void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 }
--- spring-petclinic/petclinicWeb/src/main/java/com/pulak/springframework/petclinic/boottrap/DataInit.java
+++ spring-petclinic/petclinicWeb/src/main/java/com/pulak/springframework/petclinic/boottrap/DataInit.java
@@ -20,14 +20,12 @@ public class DataInit implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Raju");
         owner1.setLastName("Raja");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Kuki");
         owner2.setLastName("Naga");
 
@@ -35,14 +33,12 @@ public class DataInit implements CommandLineRunner {
         System.out.println("Loading Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Wadde");
         vet1.setLastName("Lok");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Motte");
         vet2.setLastName("Rok");
 
