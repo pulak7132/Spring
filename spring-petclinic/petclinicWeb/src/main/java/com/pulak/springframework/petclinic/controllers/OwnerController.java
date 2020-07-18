4807738c48c555020539a0669025659c6cf0472d
pulak7132 <pulakranchal@outlook.com>
2020-07-18 09:35:37 -0400

    1. Addition of fetching of owner list from the map service in the controller. 2. Closes #16.

--- spring-petclinic/petclinicWeb/src/main/java/com/pulak/springframework/petclinic/controllers/OwnerController.java
+++ spring-petclinic/petclinicWeb/src/main/java/com/pulak/springframework/petclinic/controllers/OwnerController.java
@@ -1,12 +1,25 @@
 package com.pulak.springframework.petclinic.controllers;
 
+import com.pulak.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.Collectors;
+
 @RequestMapping("owners")
 @Controller
 public class OwnerController {
+    OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("oList", ownerService.findAll().stream().
+                sorted((x,y)->x.getId().compareTo(y.getId())).collect(Collectors.toList()));
         return "owners/ownerlist";
     }
 }
