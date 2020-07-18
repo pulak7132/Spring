cabec604ffd2c7d3cb035f8153a6e40f0321925c
pulak7132 <pulakranchal@outlook.com>
2020-07-18 10:07:26 -0400

    1. Addition of fetching of vet list from the map service in the vet controller. 2. Closes #15.

--- spring-petclinic/petclinicWeb/src/main/java/com/pulak/springframework/petclinic/controllers/VetController.java
+++ spring-petclinic/petclinicWeb/src/main/java/com/pulak/springframework/petclinic/controllers/VetController.java
@@ -1,12 +1,25 @@
 package com.pulak.springframework.petclinic.controllers;
 
+import com.pulak.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.Collectors;
+
 @RequestMapping("vets")
 @Controller
 public class VetController {
+    VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index", "/index.hmtl"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vList", vetService.findAll().stream().
+                sorted((x,y)->x.getId().compareTo(y.getId())).collect(Collectors.toList()));
         return "vets/vetlist";
     }
 }
