f4262b65bd901a3d08cceaa5174f5066bf0df934
pulak7132 <pulakranchal@outlook.com>
2020-07-31 09:14:04 -0400

    Addition of Specialty class to model and specialty attribute to Vet class. Closes #27.

--- spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Vet.java
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Vet.java
@@ -1,6 +1,16 @@
 package com.pulak.springframework.petclinic.model;
 
-import com.pulak.springframework.petclinic.model.Person;
+import java.util.Set;
 
 public class Vet extends Person {
+    Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
 }
