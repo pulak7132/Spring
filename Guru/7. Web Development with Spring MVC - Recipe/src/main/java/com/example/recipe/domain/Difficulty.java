2b6e12bb800df0f8cf6611b7d04cc1b18025a6ea
pulak7132 <pulakranchal@outlook.com>
2020-08-21 14:41:37 -0400

    Addition of Category Entity and Difficulty Enum and JPA mappings.

---
+++ Guru/7. Web Development with Spring MVC - Recipe/src/main/java/com/example/recipe/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.example.recipe.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- Guru/7. Web Development with Spring MVC - Recipe/src/main/java/com/example/recipe/domain/Recipe.java
+++ Guru/7. Web Development with Spring MVC - Recipe/src/main/java/com/example/recipe/domain/Recipe.java
@@ -16,10 +16,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-//    private Difficulty difficulty
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
-
+    @ManyToMany
+    private Set<Category> categories;
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -87,6 +89,30 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     public Byte[] getImage() {
