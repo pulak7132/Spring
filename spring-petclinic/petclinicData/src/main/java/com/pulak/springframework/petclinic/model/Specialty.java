e144e11366de5486b29711e911f9305d9fd613c6
pulak7132 <pulakranchal@outlook.com>
2020-08-23 08:44:13 -0400

    Addition of JPA annotations to Vet and Specialty Entities. Closes #37.

--- spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Specialty.java
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Specialty.java
@@ -1,5 +1,8 @@
 package com.pulak.springframework.petclinic.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Specialty  extends BaseEntity{
     String description;
 
--- spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Vet.java
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/model/Vet.java
@@ -1,9 +1,13 @@
 package com.pulak.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     Set<Specialty> specialties;
 
     public Vet() {
