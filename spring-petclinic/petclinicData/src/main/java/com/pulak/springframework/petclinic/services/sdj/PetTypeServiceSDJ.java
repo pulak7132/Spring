c8591b7f46f69db1922134af02398e8e280be00b
pulak7132 <pulakranchal@outlook.com>
2020-08-24 20:17:13 -0400

    Created Spring Data JPA Service for Vets. Closes #41.

---
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/services/sdj/PetTypeServiceSDJ.java
@@ -0,0 +1,48 @@
+package com.pulak.springframework.petclinic.services.sdj;
+
+import com.pulak.springframework.petclinic.model.PetType;
+import com.pulak.springframework.petclinic.repositories.PetTypeRepository;
+import com.pulak.springframework.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+@Service
+@Profile("SDJ")
+@Primary
+public class PetTypeServiceSDJ implements PetTypeService {
+    private PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceSDJ(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
---
+++ spring-petclinic/petclinicData/src/main/java/com/pulak/springframework/petclinic/services/sdj/VetServiceSDJ.java
@@ -0,0 +1,49 @@
+package com.pulak.springframework.petclinic.services.sdj;
+
+import com.pulak.springframework.petclinic.model.Vet;
+import com.pulak.springframework.petclinic.repositories.VetRepository;
+import com.pulak.springframework.petclinic.services.VetService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("SDJ")
+@Primary
+public class VetServiceSDJ implements VetService {
+    VetRepository vetRepository;
+
+    public VetServiceSDJ(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
