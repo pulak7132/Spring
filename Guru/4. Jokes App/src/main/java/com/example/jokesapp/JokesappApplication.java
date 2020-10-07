7740b9b5c52619fd0c92d6c966d66fc5e05d5088
pulak7132 <pulakranchal@outlook.com>
2020-10-07 12:19:07 -0400

    Moving configuration from Java to XML in module 4.

--- Guru/4. Jokes App/src/main/java/com/example/jokesapp/JokesappApplication.java
+++ Guru/4. Jokes App/src/main/java/com/example/jokesapp/JokesappApplication.java
@@ -2,9 +2,10 @@ package com.example.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//@ImportResource("classpath:chuck-config.xml")
+@ImportResource("classpath:chuck-config.xml")
 public class JokesappApplication {
 
     public static void main(String[] args) {
